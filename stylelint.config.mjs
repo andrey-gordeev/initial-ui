b4f72ceca4c4edb020b1fcac9079a3da068b3049
Andrey Gordeev <andrey.gordeev@gmail.com>
2025-09-23 02:09:21 +0100

    Add postcss-scss for parsing // comments and fix Stylelint

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: ['stylelint-config-recess-order'],
+  customSyntax: 'postcss-scss',
   plugins: ['stylelint-plugin-logical-css'],
+  extends: ['stylelint-config-recess-order'],
   rules: {
     'plugin/use-logical-properties-and-values': [true, { severity: 'warning' }],
     'plugin/use-logical-units': [true, { severity: 'warning' }],
