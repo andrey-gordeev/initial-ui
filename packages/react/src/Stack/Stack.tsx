50669225d6b36ebba4df09af45956f3ce850836b
Andrey Gordeev <andrey.gordeev@gmail.com>
2025-10-17 03:27:08 +0100

    Add `direction` prop to Stack component

--- packages/react/src/Stack/Stack.tsx
+++ packages/react/src/Stack/Stack.tsx
@@ -1,6 +1,12 @@
+import clsx from 'clsx';
 import { StackProps } from './types';
 import './styles.css';
 
-export const Stack = ({ children }: StackProps) => {
-    return <div className="stack">{children}</div>;
+export const Stack = ({ children, direction = 'horizontal' }: StackProps) => {
+    const className = clsx('stack', {
+        'stack--direction-horizontal': direction === 'horizontal',
+        'stack--direction-vertical': direction === 'vertical',
+    });
+
+    return <div className={className}>{children}</div>;
 };
--- packages/react/src/Stack/types.ts
+++ packages/react/src/Stack/types.ts
@@ -2,4 +2,5 @@ import { ReactNode } from 'react';
 
 export type StackProps = {
     children?: ReactNode;
+    direction?: 'horizontal' | 'vertical';
 };
