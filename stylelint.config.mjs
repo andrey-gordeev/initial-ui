65223df786e3599c50f7b276fe122a70fde8a68e
Andrey Gordeev <andrey.gordeev@gmail.com>
2026-04-10 04:09:44 +0100

    update CSS property sort order and reformat styles

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('stylelint').Config} */
 export default {
   customSyntax: 'postcss-scss',
-  plugins: ['stylelint-plugin-logical-css'],
-  extends: ['stylelint-config-recess-order'],
+  plugins: ['stylelint-order', 'stylelint-plugin-logical-css'],
+  extends: ['stylelint-config-pepelsbey'],
   rules: {
     'plugin/use-logical-properties-and-values': [true, { severity: 'warning' }],
     'plugin/use-logical-units': [true, { severity: 'warning' }],
